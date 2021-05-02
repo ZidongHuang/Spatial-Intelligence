780d6ce887a449f281a4bc585d4a1e4f725bdda7
Fei Xiong <xiongjjlj@outlook.com>
2021-05-02 15:31:32 +0800

    port

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ app.use(function(err, req, res, next) {
 });
 
 // port config
-const port = 8000; // config variable
+const port = 5000; // config variable
 const server = http.Server(app);
 server.listen(port, function() {
   console.log('Server running on port: ' + port);
